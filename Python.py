db73c02f30353a8992a906b7e9f1e509ce8aae27
Srihari <Sriharivernekar07@gmail.com>
2024-11-16 22:47:08 -0800

    Modified the python.py file and added a new Readme file

--- Python.py
+++ Python.py
@@ -2,3 +2,4 @@ n = int(input("Enter the first N even Numbers : "))
 for i in range(n):
     if i%2 == 0:
         print(i, end=" ")
+print("THANK YOU!")
---
+++ Readme.md
@@ -0,0 +1,2 @@
+So this is my new GitHub Repo where i share my all the project works done by me using python.
+Thank You.
